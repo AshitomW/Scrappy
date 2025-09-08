e086215697573d0a802ef9b651703d9394fe6fb1
Ashitom <sasukeuchia32828@gmail.com>
2025-09-08 17:31:27 +0545

    update execution page

--- src/app/workflow/runs/[workflowId]/[executionId]/_components/ExecutionViewer.tsx
+++ src/app/workflow/runs/[workflowId]/[executionId]/_components/ExecutionViewer.tsx
@@ -1,11 +1,21 @@
 "use client";
 import GetWorkflowExecutionWithPhases from "@/actions/workflows/GetWorkflowExecutionWithPhases";
+import { Button } from "@/components/ui/button";
+import { Separator } from "@/components/ui/separator";
 import { ExecutionStatus } from "@/types/workflow";
-import { ExecutionPhase } from "@prisma/client";
+import { Badge } from "@/components/ui/badge";
 import { useQuery } from "@tanstack/react-query";
 import { formatDistanceToNow } from "date-fns";
-import { CalendarIcon, CircleDashedIcon } from "lucide-react";
-import React from "react";
+import {
+  CalendarIcon,
+  Circle,
+  CircleDashedIcon,
+  ClockIcon,
+  CoinsIcon,
+  LucideIcon,
+  WorkflowIcon,
+} from "lucide-react";
+import React, { ReactNode } from "react";
 
 type ExecutionData = Awaited<ReturnType<typeof GetWorkflowExecutionWithPhases>>;
 
@@ -22,36 +32,86 @@ export default function ExecutionViewer({
       query.state.data?.status === ExecutionStatus.Running ? 1000 : false,
   });
 
+  const duration = DateToDurationString(
+    query.data?.startedAt,
+    query.data?.completedAt
+  );
+
   return (
     <div className="flex w-full h-full ">
       <aside className="w-[400px] min-w-[400px] max-w-[400px] border-r-2 border-separate flex flex-grow flex-col overflow-hidden">
         <div className="px-2 py-4">
-          {/*Status Label*/}
-          <div className="flex justify-between items-center py-2 px-4 text-sm">
-            <div className="text-muted-foreground flex items-center gap-2 ">
-              <CircleDashedIcon size={20} className="stroke-muted-foreground" />
-              <span>Status</span>
-            </div>
-            <div className="font-semibold capitalize flex items-center gap-2">
-              {query.data?.status}
-            </div>
-          </div>
-          {/*Started At*/}
-          <div className="flex justify-between items-center py-2 px-4 text-sm">
-            <div className="text-muted-foreground flex items-center gap-2">
-              <CalendarIcon size={20} className="stroke-muted-foreground/80" />
-              <span>Started At</span>
-            </div>
-            <div className="font-semibold capitalize flex items-center gap-2">
-              {query.data?.startedAt
+          <ExecutionLabel
+            icon={CircleDashedIcon}
+            label="Status"
+            value={query.data?.status}
+          />
+          <ExecutionLabel
+            icon={CalendarIcon}
+            label="Started At"
+            value={
+              query.data?.startedAt
                 ? formatDistanceToNow(new Date(query.data.startedAt), {
                     addSuffix: true,
                   })
-                : "-"}
-            </div>
+                : "-"
+            }
+          />
+          <ExecutionLabel icon={ClockIcon} label="Duration" value={"Todo"} />
+          <ExecutionLabel
+            icon={CoinsIcon}
+            label="Credits Consumed"
+            value={"Todo"}
+          />
+        </div>
+        <Separator />
+        <div className="flex items-center justify-center py-2 px-4">
+          <div className="text-muted-foreground flex items-center gap-2">
+            <WorkflowIcon size={20} className="stroke-muted-foreground/80" />
+            <span className="font-semibold">Phases</span>
           </div>
         </div>
+        <Separator />
+        <div className="overflow-auto h-full px-2 py-4">
+          {query.data?.phases.map((phase, index) => {
+            return (
+              <Button
+                key={phase.id}
+                className="w-full justify-between "
+                variant={"ghost"}
+              >
+                <div className="flex items-center gap-2">
+                  <Badge variant={"outline"}>{index + 1}</Badge>
+                  <p className="font-semibold">{phase.name}</p>
+                </div>
+              </Button>
+            );
+          })}
+        </div>
       </aside>
+    </div>
+  );
+}
+
+function ExecutionLabel({
+  icon,
+  label,
+  value,
+}: {
+  icon: LucideIcon;
+  label: ReactNode;
+  value: ReactNode;
+}) {
+  const Icon = icon;
+  return (
+    <div className="flex justify-between items-center py-2 px-4 text-sm">
+      <div className="text-muted-foreground flex items-center gap-2">
+        <Icon size={20} className="stroke-muted-foreground/80" />
+        <span>{label}</span>
+      </div>
+      <div className="font-semibold capitalize flex gap-2 items-center">
+        {value}
+      </div>
     </div>
   );
 }
