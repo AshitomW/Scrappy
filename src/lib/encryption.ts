5e8cbe1b0b2d79143158e520ed82ca57e43f7ffc
Ashitom <sasukeuchia32828@gmail.com>
2025-09-24 10:04:07 +0545

    encryption and decryption

---
+++ src/lib/encryption.ts
@@ -0,0 +1,40 @@
+import crypto from "crypto";
+import { init } from "next/dist/compiled/webpack/webpack";
+
+const ALGORITHM = "aes-256-cbc";
+
+export function symmetricEncrypt(data: string) {
+  const key = process.env.ENCRYPTION_KEY;
+  if (!key) throw new Error("Encryption Key Not Found!");
+
+  const initVec = crypto.randomBytes(16);
+  const cipher = crypto.createCipheriv(
+    ALGORITHM,
+    Buffer.from(key, "hex"),
+    initVec
+  );
+
+  let encrypted = cipher.update(data);
+  encrypted = Buffer.concat([encrypted, cipher.final()]);
+
+  return initVec.toString("hex") + ":" + encrypted.toString("hex");
+}
+
+export function symmetricDecrypt(data: string) {
+  const key = process.env.ENCRYPTION_KEY;
+  if (!key) throw new Error("Encryption Key Not Found!");
+
+  const textParts = data.split(":");
+  const initVec = Buffer.from(textParts.shift() as string, "hex");
+  const encryptedText = Buffer.from(textParts.join(":"), "hex");
+
+  const decipher = crypto.createDecipheriv(
+    ALGORITHM,
+    Buffer.from(key, "hex"),
+    initVec
+  );
+
+  let decrypted = decipher.update(encryptedText);
+  decrypted = Buffer.concat([decrypted, decipher.final()]);
+  return decrypted.toString();
+}
