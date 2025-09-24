8363fd2d802b9e01c4c0f695aa0254cb98aba0a1
Ashitom <sasukeuchia32828@gmail.com>
2025-09-24 14:21:09 +0545

    use gemini as ai for extraction

--- src/lib/workflow/Executor/ExtractWithAiExecutor.ts
+++ src/lib/workflow/Executor/ExtractWithAiExecutor.ts
@@ -4,7 +4,7 @@ import { ClickElementTask } from "../tasks/click_element";
 import { ExtractDataWithAITask } from "../tasks/Extract_Data_With_AI";
 import { prisma } from "@/lib/prisma";
 import { symmetricDecrypt } from "@/lib/encryption";
-import { GoogleGenAI } from "@google/genai";
+import { OpenAI } from "openai";
 export async function ExtractDataWithAIExecutor(
   environment: ExecutionEnvironment<typeof ExtractDataWithAITask>
 ): Promise<boolean> {
@@ -40,11 +40,44 @@ export async function ExtractDataWithAIExecutor(
       return false;
     }
 
-    const ai = new GoogleGenAI({ apiKey: plainCredentials });
-    const response = await ai.models.generateContent({
-      model: "gemini-2.5-flash",
-      contents: prompt,
+    const openai = new OpenAI({
+      apiKey: plainCredentials,
+      baseURL: "https://generativelanguage.googleapis.com/v1beta/openai/",
     });
+
+    const response = await openai.chat.completions.create({
+      model: "gemini-2.0-flash",
+      messages: [
+        {
+          role: "system",
+          content:
+            "You are a webscraper helper that extracts idata from HTML or text. You will be given a piece of text or HTML content as input and also the prompt with the data you want to extract. The response should always be only the extracted data as a JSON array or object, without any additional words or explanations. Analyze the input carefully and extract data precisely based on the prompt. If no data is found, return an empty JSON array. Work only with the provided content and ensure the output is always a valid JSON array without any surrounding.",
+        },
+        {
+          role: "user",
+          content: content,
+        },
+        {
+          role: "user",
+          content: prompt,
+        },
+      ],
+      temperature: 1,
+    });
+
+    environment.log.info(`Prompt Tokens : ${response.usage?.prompt_tokens}`);
+    environment.log.info(
+      `Completion Tokens : ${response.usage?.completion_tokens}`
+    );
+
+    const result = response.choices[0].message?.content;
+    console.log("@@RESULT: ", result);
+    if (!result) {
+      environment.log.error("empty response from ai");
+      return false;
+    }
+
+    environment.setOutput("Extracted Data", result);
 
     return true;
   } catch (error: any) {
